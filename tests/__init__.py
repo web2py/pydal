9a061d7b13482046509c6c231b0018a600cb9155
gi0baro <giovanni.barillari@gmail.com>
2014-12-12 18:21:26 +0100

    Fix wrong import in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,6 @@ NOSQL = any([name in (os.getenv("DB") or "")
             for name in ("datastore", "mongodb", "imap")])
 
 if NOSQL:
-    from test_dal_nosql import *
+    from nosql import *
 else:
     from sql import *
