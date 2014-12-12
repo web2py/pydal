11fe6f7b4acbdde023169466c089eadf4591a24a
gi0baro <giovanni.barillari@gmail.com>
2014-12-12 15:54:05 +0100

    Activated sql-nosql pick in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
 import os
 
-#NOSQL = any([name in (os.getenv("DB") or "")
-#            for name in ("datastore", "mongodb", "imap")])
+NOSQL = any([name in (os.getenv("DB") or "")
+            for name in ("datastore", "mongodb", "imap")])
 
-#if NOSQL:
-#    from test_dal_nosql import *
-#else:
-from sql import *
+if NOSQL:
+    from test_dal_nosql import *
+else:
+    from sql import *
