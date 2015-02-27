9bc003583a164c59d77f31c7c31e22ab81c259ee
gi0baro <giovanni.barillari@gmail.com>
2015-02-27 12:22:46 +0100

    Releasing 15.02.27 bug fix release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Links
 from setuptools import setup
 setup(
     name='pyDAL',
-    version='15.02',
+    version='15.02.27',
     url='https://github.com/web2py/pydal',
     license='BSD',
     author='Massimo Di Pierro',
