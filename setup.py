b0a2c77fc43f5086d8c6fc2dc05d017deb44af0f
gi0baro <giovanni.barillari@gmail.com>
2015-03-23 20:40:29 +0100

    Releasing 15.03

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Links
 from setuptools import setup
 setup(
     name='pyDAL',
-    version='15.03-dev',
+    version='15.03',
     url='https://github.com/web2py/pydal',
     license='BSD',
     author='Massimo Di Pierro',
