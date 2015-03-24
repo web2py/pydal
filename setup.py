e55496bd56c320b310a64047b0305ca3ac2b3c24
gi0baro <giovanni.barillari@gmail.com>
2015-03-24 11:48:14 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Links
 from setuptools import setup
 setup(
     name='pyDAL',
-    version='15.03',
+    version='15.04-dev',
     url='https://github.com/web2py/pydal',
     license='BSD',
     author='Massimo Di Pierro',
