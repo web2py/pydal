f10759fc0779cdecb70733cdbde82a2a8bc50a37
ilvalle <paolo.valleri@gmail.com>
2015-04-25 19:01:51 +0200

    dropped contrib/simplejson

--- pydal/_load.py
+++ pydal/_load.py
@@ -5,10 +5,12 @@ try:
     from collections import OrderedDict
 except:
     from .contrib.ordereddict import OrderedDict
+    
 try:
+# try external module
+    import simplejson as json
+except ImportError:
     import json
-except:
-    from .contrib import simplejson as json
 
 from .contrib import portalocker
 # TODO: uncomment the lines below when contrib/portalocker will be
