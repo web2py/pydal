6480b27f7aa4cc3ba10c81758743219ce3cf92f4
ilvalle <paolo.valleri@gmail.com>
2015-05-25 18:20:37 +0200

    fix Row.get('table.field'), close web2py/pydal#197

--- tests/sql.py
+++ tests/sql.py
@@ -1828,7 +1828,9 @@ class TestLazy(unittest.TestCase):
         self.assertEqual(row.value, None)
         self.assertEqual(row[db.tt.value], None)
         self.assertEqual(row['tt.value'], None)
+        self.assertEqual(row.get('tt.value'), None)
         self.assertEqual(row['value'], None)
+        self.assertEqual(row.get('value'), None)
         db.tt.drop()
         db.close()
 
