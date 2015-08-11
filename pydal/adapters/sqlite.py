443b54c1edc8d57480cd9c664c67c4f70f316c4d
Dominic König <dominic@nursix.org>
2015-08-11 13:07:22 +0200

    Fix for SQLiteAdapter: would have raised an exception with REGEXP if item is None

--- pydal/adapters/sqlite.py
+++ pydal/adapters/sqlite.py
@@ -41,6 +41,8 @@ class SQLiteAdapter(BaseAdapter):
 
     @staticmethod
     def web2py_regexp(expression, item):
+        if item is None:
+            return False
         return re.compile(expression).search(item) is not None
 
     def __init__(self, db, uri, pool_size=0, folder=None, db_codec ='UTF-8',
