777e1803c1761c3f319cdd6d0cf69d281d004e7f
Jason <jphillips@ua.edu>
2017-05-03 09:18:17 -0500

    rewrite oracle adapter in response to tests

--- pydal/adapters/oracle.py
+++ pydal/adapters/oracle.py
@@ -1,8 +1,11 @@
 import re
 import sys
+import os
 from .._compat import integer_types, long
 from ..helpers.classes import Reference
+from ..helpers.methods import use_common_filters
 from .base import SQLAdapter
+from ..objects import Table, Field, Expression, Query
 from . import adapters, with_connection_or_raise
 
 @adapters.register_for('oracle')
@@ -11,7 +14,7 @@ class Oracle(SQLAdapter):
     drivers = ('cx_Oracle',)
 
     cmd_fix = re.compile(
-        "[^']*('[^']*'[^']*)*\:(?P<clob>CLOB\('([^']+|'')*'\))")
+        "[^']*('[^']*'[^']*)*\:(?P<clob>(C|B)LOB\('([^']+|'')*'\))")
 
     def _initialize_(self, do_connect):
         super(Oracle, self)._initialize_(do_connect)
@@ -37,19 +40,22 @@ class Oracle(SQLAdapter):
         command = self.filter_sql_command(args[0])
         i = 1
         while True:
-            m = self.oracle_fix.match(command)
+            m = self.cmd_fix.match(command)
             if not m:
                 break
             command = command[:m.start('clob')] + str(i) + \
                 command[m.end('clob'):]
-            args.append(m.group('clob')[6:-2].replace("''", "'"))
+            args = args + (m.group('clob')[6:-2].replace("''", "'"),)
             i += 1
         if command[-1:] == ';':
             command = command[:-1]
         handlers = self._build_handlers_for_execution()
         for handler in handlers:
             handler.before_execute(command)
-        rv = self.cursor.execute(command, *args[1:], **kwargs)
+        if (len(args)>1):
+            rv = self.cursor.execute(command, args[1:], **kwargs)
+        else:
+            rv = self.cursor.execute(command, **kwargs)
         for handler in handlers:
             handler.after_execute(command)
         return rv
@@ -59,19 +65,27 @@ class Oracle(SQLAdapter):
         self.execute('SELECT %s.currval FROM dual;' % sequence_name)
         return long(self.cursor.fetchone()[0])
 
+    def sqlsafe_table(self, tablename, original_tablename=None):
+        if original_tablename is not None:
+            return self.dialect.alias(original_tablename, tablename)
+        return self.dialect.quote(tablename)
+
     def create_sequence_and_triggers(self, query, table, **args):
         tablename = table._rname
+        if not '_id' in table:
+            return self.execute(query)
         id_name = table._id._rname
         sequence_name = table._sequence_name
         trigger_name = table._trigger_name
         self.execute(query)
-        self.execute(
-            'CREATE SEQUENCE %s START WITH 1 INCREMENT BY 1 NOMAXVALUE ' +
-            'MINVALUE -1;' % sequence_name)
+        self.execute("""
+            CREATE SEQUENCE %s START WITH 1 INCREMENT BY 1 NOMAXVALUE 
+            MINVALUE -1;""" % sequence_name)
         self.execute(_trigger_sql % dict(
-            trigger_name=trigger_name, tablename=tablename,
-            sequence_name=sequence_name,
-            id=id_name)
+            trigger_name=self.dialect.quote(trigger_name), 
+            tablename=self.dialect.quote(tablename),
+            sequence_name=self.dialect.quote(sequence_name),
+            id=self.dialect.quote(id_name))
         )
 
     def fetchall(self):
@@ -86,24 +100,56 @@ class Oracle(SQLAdapter):
 
     def sqlsafe_table(self, tablename, original_tablename=None):
         if original_tablename is not None:
-            return (
-                self.dialect.quote_template + ' ' +
-                self.dialect.quote_template
-            ) % (original_tablename, tablename)
+            return '%s %s' % (
+                self.dialect.quote(original_tablename), 
+                self.dialect.quote(tablename)
+            )
         return self.dialect.quote(tablename)
+
+    def _expand(self, expression, field_type=None, colnames=False, query_env={}):
+        # override default expand to ensure quoted fields
+        if isinstance(expression, Field):
+            if not colnames:
+                rv = self.dialect.sqlsafe(expression)
+            else:
+                rv = self.dialect.longname(expression)
+            if field_type == 'string' and expression.type not in (
+                    'string', 'text', 'json', 'password'):
+                rv = self.dialect.cast(rv, self.types['text'], query_env)
+            return str(rv)
+        else:
+            return super(Oracle, self)._expand(expression, field_type, colnames, query_env)
+
+    def expand(self, expression, field_type=None, colnames=False,
+        query_env={}):
+        return self._expand(expression, field_type, colnames, query_env)
 
     def _build_value_for_insert(self, field, value, r_values):
         if field.type is 'text':
-            r_values[':' + field._rname] = self.expand(value, field.type)
-            return ':' + field._rname
+            _rname = (field._rname[1]=='"') and field._rname[1:-1] or field._rname
+            r_values[_rname] = value
+            return ':' + _rname
         return self.expand(value, field.type)
+
+    def _update(self, table, query, fields):
+        sql_q = ''
+        query_env = dict(current_scope=[table._tablename])
+        if query:
+            if use_common_filters(query):
+                query = self.common_filter(query, [table])
+            sql_q = self.expand(query, query_env=query_env)
+        sql_v = ','.join([
+            '%s=%s' % (self.dialect.quote(field._rname),
+                self.expand(value, field.type, query_env=query_env))
+            for (field, value) in fields])
+        return self.dialect.update(table, sql_v, sql_q)
 
     def _insert(self, table, fields):
         if fields:
             r_values = {}
             return self.dialect.insert(
                 table._rname,
-                ','.join(el[0]._rname for el in fields),
+                ','.join(self.dialect.quote(el[0]._rname) for el in fields),
                 ','.join(
                     self._build_value_for_insert(f, v, r_values)
                     for f, v in fields)
@@ -116,7 +162,10 @@ class Oracle(SQLAdapter):
             if not values:
                 self.execute(query)
             else:
-                self.execute(query, *values)
+                if type(values)==dict:
+                    self.execute(query, **values)
+                else:
+                    self.execute(query, values)
         except:
             e = sys.exc_info()[1]
             if hasattr(table, '_on_insert_error'):
@@ -137,21 +186,31 @@ class Oracle(SQLAdapter):
         (rid._table, rid._record) = (table, None)
         return rid
 
+    def _regex_select_as_parser(self, colname):
+        return re.compile('\s+"(\S+)"').search(colname)
+
+    def parse(self, rows, fields, colnames, blob_decode=True, cacheable=False):
+        if len(rows) and len(rows[0]) == len(fields) + 1 and type(rows[0][-1])==int:
+            # paging has added a trailing rownum column to be discarded
+            rows = [row[:-1] for row in rows]
+        return super(Oracle, self).parse(rows, fields, colnames, blob_decode, cacheable)
+
 _trigger_sql = """
-CREATE OR REPLACE TRIGGER %(trigger_name)s BEFORE INSERT ON %(tablename)s FOR EACH ROW
-DECLARE
-    curr_val NUMBER;
-    diff_val NUMBER;
-    PRAGMA autonomous_transaction;
-BEGIN
-    IF :NEW.%(id)s IS NOT NULL THEN
-        EXECUTE IMMEDIATE 'SELECT %(sequence_name)s.nextval FROM dual' INTO curr_val;
-        diff_val := :NEW.%(id)s - curr_val - 1;
-        IF diff_val != 0 THEN
-          EXECUTE IMMEDIATE 'alter sequence %(sequence_name)s increment by '|| diff_val;
-          EXECUTE IMMEDIATE 'SELECT %(sequence_name)s.nextval FROM dual' INTO curr_val;
-          EXECUTE IMMEDIATE 'alter sequence %(sequence_name)s increment by 1';
+    CREATE OR REPLACE TRIGGER %(trigger_name)s BEFORE INSERT ON %(tablename)s FOR EACH ROW
+    DECLARE
+        curr_val NUMBER;
+        diff_val NUMBER;
+        PRAGMA autonomous_transaction;
+    BEGIN
+        IF :NEW.%(id)s IS NOT NULL THEN
+            EXECUTE IMMEDIATE 'SELECT %(sequence_name)s.nextval FROM dual' INTO curr_val;
+            diff_val := :NEW.%(id)s - curr_val - 1;
+            IF diff_val != 0 THEN
+            EXECUTE IMMEDIATE 'alter sequence %(sequence_name)s increment by '|| diff_val;
+            EXECUTE IMMEDIATE 'SELECT %(sequence_name)s.nextval FROM dual' INTO curr_val;
+            EXECUTE IMMEDIATE 'alter sequence %(sequence_name)s increment by 1';
+            END IF;
         END IF;
-    END IF;
-    SELECT %(sequence_name)s.nextval INTO :NEW.%(id)s FROM DUAL;
-END;"""
+        SELECT %(sequence_name)s.nextval INTO :NEW.%(id)s FROM DUAL;
+    END;
+"""
