a8ee93edc07c8e29ee0063420ef614935642ea16
Leonel Câmara <leonelcamara@gmail.com>
2019-03-05 03:54:04 +0000

    Parse json (#558)
    Fixes web2py/web2py#2121

--- pydal/parsers/google.py
+++ pydal/parsers/google.py
@@ -1,10 +1,10 @@
 from ..adapters.google import GoogleDatastore
-from .base import BasicParser
+from .base import BasicParser, JSONParser
 from . import parsers, for_type
 
 
 @parsers.register_for(GoogleDatastore)
-class GoogleDatastoreParser(BasicParser):
+class GoogleDatastoreParser(BasicParser, JSONParser):
     @for_type('id')
     def _id(self, value):
         return value
