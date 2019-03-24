540d9c05dd07946e0d17e310805286293b9c1b09
mdipierro <massimo.dipierro@gmail.com>
2019-03-23 21:43:21 -0700

    better firebird regexp, thanks Paolo P.

--- pydal/adapters/firebird.py
+++ pydal/adapters/firebird.py
@@ -12,36 +12,28 @@ class FireBird(SQLAdapter):
     support_distributed_transaction = True
     commit_on_alter_table = True
 
-    REGEX_URI = re.compile(
-        '^(?P<user>[^:@]+)(\:(?P<password>[^@]*))?@(?P<host>\[[^/]+\]|' +
-        '[^\:/]+)(\:(?P<port>[0-9]+))?/(?P<db>.+?)' +
-        '(\?set_encoding=(?P<charset>\w+))?$')
+    REGEX_URI = \
+         '^(?P<user>[^:@]+)(:(?P<password>[^@]*))?' \
+        r'@(?P<host>[^:/]+)(:(?P<port>\d+))?/(?P<db>[^?]+)' \
+        r'(\?set_encoding=(?P<charset>\w+))?$'
 
     def _initialize_(self, do_connect):
         super(FireBird, self)._initialize_(do_connect)
         ruri = self.uri.split('://', 1)[1]
-        m = self.REGEX_URI.match(ruri)
+        m = re.match(self.REGEX_URI, ruri)
         if not m:
             raise SyntaxError("Invalid URI string in DAL")
         user = self.credential_decoder(m.group('user'))
-        if not user:
-            raise SyntaxError('User required')
         password = self.credential_decoder(m.group('password'))
         if not password:
             password = ''
         host = m.group('host')
-        if not host:
-            raise SyntaxError('Host name required')
         db = m.group('db')
-        if not db:
-            raise SyntaxError('Database name required')
         port = int(m.group('port') or 3050)
         charset = m.group('charset') or 'UTF8'
         self.driver_args.update(
             dsn='%s/%s:%s' % (host, port, db),
-            user=self.credential_decoder(user),
-            password=self.credential_decoder(password),
-            charset=charset)
+            user=user, password=password, charset=charset)
 
     def connector(self):
         return self.driver.connect(**self.driver_args)
@@ -58,35 +50,31 @@ class FireBird(SQLAdapter):
         self.execute(query)
         self.execute('create generator %s;' % sequence_name)
         self.execute('set generator %s to 0;' % sequence_name)
+        qid = self.dialect.quote_template % 'id'
         self.execute(
-            'create trigger %s for %s active before insert position 0 ' +
-            'as\nbegin\nif(new."id" is null) then\nbegin\n' +
-            'new."id" = gen_id(%s, 1);\nend\nend;' % (
-                trigger_name, tablename, sequence_name))
+            'create trigger %s for %s active before insert position 0 as\n' \
+            'begin\nif(new.%s is null) then new.%s = gen_id(%s, 1);\n' \
+            'end;' % (trigger_name, tablename, qid, qid, sequence_name))
 
 
 @adapters.register_for('firebird_embedded')
 class FireBirdEmbedded(FireBird):
-    REGEX_URI = re.compile(
-        '^(?P<user>[^:@]+)(\:(?P<password>[^@]*))?@(?P<path>[^\?]+)' +
-        '(\?set_encoding=(?P<charset>\w+))?$')
+    REGEX_URI = \
+         '^(?P<user>[^:@]+)(:(?P<password>[^@]*))?' \
+        r'@(?P<path>[^?]+)(\?set_encoding=(?P<charset>\w+))?$'
 
     def _initialize_(self, do_connect):
         super(FireBird, self)._initialize_(do_connect)
         ruri = self.uri.split('://', 1)[1]
-        m = self.REGEX_URI.match(ruri)
+        m = re.match(self.REGEX_URI, ruri)
         if not m:
             raise SyntaxError("Invalid URI string in DAL")
         user = self.credential_decoder(m.group('user'))
-        if not user:
-            raise SyntaxError('User required')
         password = self.credential_decoder(m.group('password'))
         if not password:
             password = ''
         pathdb = m.group('path')
-        if not pathdb:
-            raise SyntaxError('Path required')
         charset = m.group('charset') or 'UTF8'
         self.driver_args.update(
-            host='', database=pathdb, user=self.credential_decoder(user),
-            password=self.credential_decoder(password), charset=charset)
+            host='', database=pathdb,
+            user=user, password=password, charset=charset)
