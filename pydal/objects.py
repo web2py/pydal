1f13ac59bbe663fca691c8525e1f6de3baac18e0
David Orme <davidorme@users.noreply.github.com>
2019-03-24 05:10:27 +0000

    Postgre extensions to provide ST_Transform and to pass geometries to records as WKB (#567)

--- pydal/objects.py
+++ pydal/objects.py
@@ -1496,6 +1496,10 @@ class Expression(object):
         return Expression(
             self.db, self._dialect.st_astext, self, type='string')
 
+    def st_aswkb(self):
+        return Expression(
+            self.db, self._dialect.st_aswkb, self, type='string')
+
     def st_x(self):
         return Expression(self.db, self._dialect.st_x, self, type='string')
 
@@ -1514,6 +1518,10 @@ class Expression(object):
         return Expression(
             self.db, self._dialect.st_simplifypreservetopology, self, value,
             self.type)
+
+    def st_transform(self, value):
+        return Expression(
+            self.db, self._dialect.st_transform, self, value, self.type)
 
     # GIS queries
 
