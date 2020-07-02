26af6268894fd97d8e6540f91b2c01e9f88f7c77
jicho <eric@eric-op-het.net>
2020-07-02 11:09:35 +0200

    Missing "number(s)" translation
    When you're using IS_STRONG() in a different language the "number" of "numbers" text with IS_STRONG() is displayed in English.

--- pydal/validators.py
+++ pydal/validators.py
@@ -4631,6 +4631,7 @@ class IS_STRONG(Validator):
                 numbers = "number"
                 if self.number > 1:
                     numbers = "numbers"
+                numbers = self.translator(number)
                 if not len(all_number) >= self.number:
                     failures.append(
                         self.translator("Must include at least %s %s")
