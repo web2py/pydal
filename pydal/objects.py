d6ac19242dd278eef0d336183c8c3a8994c16ed8
Leonel Câmara <leonelcamara@gmail.com>
2020-10-01 18:28:46 +0100

    Fix weird FileNotFoundError using relative path
    Fix weird FileNotFoundError using relative paths that happens if the file is in a microsoft windows shared folder.

--- pydal/objects.py
+++ pydal/objects.py
@@ -2065,6 +2065,7 @@ class Field(Expression, Serializable):
                     path = self.uploadfolder
                 elif self.db is not None and self.db._adapter.folder:
                     path = pjoin(self.db._adapter.folder, "..", "uploads")
+                    path = os.path.abspath(path)
                 else:
                     raise RuntimeError(
                         "you must specify a Field(..., uploadfolder=...)"
@@ -2149,6 +2150,7 @@ class Field(Expression, Serializable):
                 path = self.uploadfolder
             else:
                 path = pjoin(self.db._adapter.folder, "..", "uploads")
+                path = os.path.abspath(path)
         if self.uploadseparate:
             t = m.group("table")
             f = m.group("field")
